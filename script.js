5c32a29ee66497d01f7d8f7e6a7e732680997f7a
Rohan <rohanmaharjan181x@gmail.com>
2023-03-09 07:57:12 +0545

    innerhtml added

--- script.js
+++ script.js
@@ -5,14 +5,20 @@ function validation()
   // document.getElementById("trigger").addEventListener("click",validation);
 var name = document.getElementById('name').value;
 var address = document.getElementById('address').value;
-if (name == ''){
-    alert('name is required');
-    document.getElementById('name').focus();
+ 
+if(name == ''){
+  //  alert('address is required');
+  document.getElementById('nameERR').innerHTML="name is required";
+  document.getElementById('name').focus();
 }
-if(address == ''){
-    alert('address is required');
+  if(address == ''){
+    //  alert('address is required');
+    document.getElementById('addressERR').innerHTML="address is required";
+    document.getElementById('address').focus();
+  }
 }
-}
+
 document.getElementById("myform").addEventListener("submit",validation);
 
 
+ 
